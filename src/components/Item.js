2b7b302fa565206ae6dc7d15441ff77861009600
Adeyemi Arimoro <arimoro71@gmail.com>
2023-05-03 12:53:01 -0500

    Add solution

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,27 @@
-import React from "react";
+import {React, useState} from "react";
 
-function Item({ name, category }) {
+function Item({item}) {
+  const [inCart, setInCart] = useState(true)
+  const { name, category, id} = item;
+
+  function handleOnClick(id){ 
+      setInCart(inCart=>!inCart)
+      if(inCart===false && item.id===parseInt(id)){
+        item.remove();  
+      }  
+    }
+  
+   const listClass = inCart? "in-cart": "";
+  console.log(inCart)
+
   return (
-    <li className="">
+    <li className={listClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={()=>handleOnClick(id)} className="add">{inCart? "Add to Cart" : "Remove from Cart" }</button>
     </li>
   );
 }
 
 export default Item;
+ 
